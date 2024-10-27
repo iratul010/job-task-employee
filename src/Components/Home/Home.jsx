da03cf171167a4dd59f807dc5df7201d2e5b4362
Ratul Islam <iratul010@gmail.com>
2024-10-27 21:31:19 +0600

    try-path

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
           </div>
         </div>
         <div className="flex justify-center items-center">
-          <img className="rounded-md" src="../../../public/img/home-bg.jpeg" alt="Home Img" />
+          <img className="rounded-md" src='https://i.imgur.com/public/img/home-bg.jpeg' alt="Home Img" />
         </div>
       </div>
     </div>
